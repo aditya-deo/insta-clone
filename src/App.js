23ecc6ed87325f57dbf97d52905379bd6425b506
TECOA136_Aditya_ Deo <ahdeo2015@gmail.com>
2022-02-16 13:27:14 +0530

    complete auth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Input from "@mui/material/Input";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const style = {
   position: "absolute",
@@ -29,6 +29,7 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState(false);
 
   //TBH I don't really understand this useEffect, I have copied code from the latest documentation (as of 15 feb 2022)
   useEffect(() => {
@@ -48,6 +49,8 @@ function App() {
       unsubscribe();
     };
   }, [user, username]);
+
+  
 
   useEffect(() => {
     const q = query(collection(db, "posts"));
@@ -76,8 +79,27 @@ function App() {
       });
   };
 
+
+  const signIn = (e) =>{
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      // const user = userCredential.user;
+      setOpenSignIn(false);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(`${errorCode} : ${errorMessage}`);
+    });
+  }
+
   return (
     <div className="App">
+
+    {/* Modal for sign Up */}
       <Modal
         open={open}
         onClose={() => {
@@ -126,6 +148,49 @@ function App() {
         </Box>
       </Modal>
 
+
+{/* Modal for sign in */}
+      <Modal
+        open={openSignIn}
+        onClose={() => {
+          setOpenSignIn(false);
+        }}
+      >
+        <Box sx={style}>
+          <form className="app__signup">
+            <center>
+              <Typography id="modal-modal-title" variant="h6" component="h1">
+                SIGN IN
+              </Typography>
+            </center>
+
+            <br></br>
+            <Input
+              fullWidth={true}
+              value={email}
+              placeholder="E-mail"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+            />
+            <Input
+              fullWidth={true}
+              value={password}
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+            />
+
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </Box>
+      </Modal>
+
+
+
       <div className="app__header">
         <img
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
@@ -135,15 +200,31 @@ function App() {
       </div>
 
       {user ? (
-        <Button>Sign Out</Button>
-      ) : (
         <Button
           onClick={() => {
-            setOpen(true);
+            auth.signOut();
           }}
         >
-          Sign Up
+          Sign Out
         </Button>
+      ) : (
+        <div class="app__loginContainer">
+          <Button
+            onClick={() => {
+              setOpenSignIn(true);
+            }}
+          >
+            Sign In
+          </Button>
+
+          <Button
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Sign Up
+          </Button>
+        </div>
       )}
 
       {posts.map(({ id, post }) => {
